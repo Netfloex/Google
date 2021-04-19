419ce577d9bf1c384fcb10302cdc4a9af1a80a37
Netfloex <netfloex@gmail.com>
2021-04-19 14:25:16 +0200

    API Keys in .env

--- src/api/google.ts
+++ src/api/google.ts
@@ -1,5 +1,5 @@
 import { Search } from "../@types/google";
 
-const url = "https://www.googleapis.com/customsearch/v1?key=AIzaSyBNhRzrLNGcHHfyLZcV6EIZqa79H31YlmI&cx=17fcef79b9f3bd97b&q=";
+const url = `https://www.googleapis.com/customsearch/v1?key=${process.env.search_key}&cx=${process.env.custom_search_key}&q=`;
 
 export const search = async (query: string): Promise<Search> => await fetch(url + query).then(r => r.json());
